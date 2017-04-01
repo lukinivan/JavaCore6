fc0051914b5d2d6dc97d790ea7307a38192da044
lukinivan <Lukinivan09@gmail.com>
2017-04-01 14:39:08 +0300

    module 2, Home Work 3, first comit.

---
+++ Modul_2/Task_3/Bank_2.java
@@ -0,0 +1,40 @@
+/*
+* Main
+* Task 3.1
+* Lukin Ivan
+ */
+
+package HomeWork.Modul_2.Task_3;
+
+class Bank_2 {
+    private int[] balances;
+    private String[] ownerNames;
+
+    Bank_2(int[] balances, String[] ownerNames) {
+        this.balances = balances;
+        this.ownerNames = ownerNames;
+    }
+
+    String outPut(String name, double sum) {
+        double commission = 0.05;
+        for (int i = 0; i < ownerNames.length; i++) {
+            if (ownerNames[i].equals(name)) {
+                if ((commission * sum) + sum < balances[i]) {
+                    balances[i] -= (commission * sum) + sum;
+                    return (name + " - " + "Commission: " + (commission * sum) + ", " + "Balsnce: " + balances[i]);
+                }
+                return ("User " + name + " not enough money");
+            }
+        }
+        return ("The user " + name + " is not found");
+    }
+
+    String getBalances(String name) {
+        for (int i = 0; i < ownerNames.length; i++) {
+            if (ownerNames[i].equals(name)) {
+                return ("Owner " + name + " have - " + balances[i]);
+            }
+        }
+        return "The user " + name + " is not found";
+    }
+}
---
+++ Modul_2/Task_3/Main.java
@@ -0,0 +1,21 @@
+/*
+* Main
+* Task 3.1
+* Lukin Ivan
+ */
+package HomeWork.Modul_2.Task_3;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] balances = {1200, 250, 2000, 500, 3200};
+        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+        Bank_2 Owner = new Bank_2(balances, ownerNames);
+
+        System.out.println(Owner.outPut("Jane", 200));
+        System.out.println(Owner.outPut("Jack", 2000));
+        System.out.println(Owner.outPut("Ivan", 200));
+
+        System.out.println(Owner.getBalances("Oww"));
+    }
+}
