8ffed4111214c614c486d4dd9d33be1f9e4ae64b
lukinivan <Lukinivan09@gmail.com>
2017-04-02 11:19:15 +0300

    module 3, Home Work 1, first comit.

---
+++ Module_3/Task_1/Bird.java
@@ -0,0 +1,19 @@
+/*
+* Bird
+* Task 3.1
+* Lukin Ivan
+ */
+
+package HomeWork.Module_3.Task_1;
+
+class Bird {
+    Bird() {
+    }
+
+    void sing() {
+        System.out.println("I am walking");
+        System.out.println("I am flying");
+        System.out.println("I am singing");
+        System.out.println("I am Bird");
+    }
+}
---
+++ Module_3/Task_1/Main.java
@@ -0,0 +1,14 @@
+/*
+* Main
+* Task 3.1
+* Lukin Ivan
+ */
+
+package HomeWork.Module_3.Task_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.sing();
+    }
+}
