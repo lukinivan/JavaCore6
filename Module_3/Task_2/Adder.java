ca06bdd2d808d6054b3dc30677220008104a732f
lukinivan <Lukinivan09@gmail.com>
2017-04-02 11:41:17 +0300

    module 3, Task 2, first comit.

---
+++ Module_3/Task_2/Adder.java
@@ -0,0 +1,16 @@
+/*
+* Adder
+* Module Task 2.1
+* Lukin Ivan
+ */
+
+package HomeWork.Module_3.Task_2;
+
+public class Adder extends Arithmetic {
+    public Adder() {
+    }
+
+    boolean check(Integer a, Integer b){
+        return a >= b;
+    }
+}
---
+++ Module_3/Task_2/Arithmetic.java
@@ -0,0 +1,16 @@
+/*
+* Arithmetic
+* Module Task 2.1
+* Lukin Ivan
+ */
+
+package HomeWork.Module_3.Task_2;
+
+class Arithmetic {
+    Arithmetic() {
+    }
+
+    int add(Integer a, Integer b){
+        return a+b;
+    }
+}
