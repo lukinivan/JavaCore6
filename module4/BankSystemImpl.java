8155cba4ba234a126829a00718c52831c2ad439d
lukinivan <Lukinivan09@gmail.com>
2017-04-12 13:55:51 +0300

    Домашка 4.5

--- module4/BankSystemImpl.java
+++ module4/BankSystemImpl.java
@@ -3,17 +3,22 @@ package HomeWork.module4;
 public class BankSystemImpl implements BankSystem {
     @Override
     public void withdrawOfUser(User user, int amount) {
+        if (amount != 0 && user != null) {
+            user.setBalance(user.getBalance() - amount);
+        }
 
     }
 
     @Override
     public void fundUser(User user, int amount) {
-
+        if (amount != 0 && user != null) {
+            user.setBalance(user.getBalance() + amount);
+        }
     }
 
     @Override
     public void transferMoney(User fromUser, User toUser, int amount) {
-        if(amount!=0) {
+        if (amount != 0 && fromUser != null && toUser != null) {
             fromUser.setBalance(fromUser.getBalance() - amount);
             toUser.setBalance(toUser.getBalance() + amount);
         }
@@ -21,6 +26,8 @@ public class BankSystemImpl implements BankSystem {
 
     @Override
     public void paySalary(User user) {
-        user.setBalance(user.getBalance() + user.getSalary());
+        if (user != null) {
+            user.setBalance(user.getBalance() + user.getSalary());
+        }
     }
 }
